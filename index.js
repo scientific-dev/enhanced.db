f315dad0c2863727304ead2e498df8f1b03b9169
Science Spot <69678617+Scientific-Guy@users.noreply.github.com>
2020-09-01 21:10:19 +0530

    More options and methods

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
+const fs = require('fs')
 const startedAt = Date.now()
+const db = new (require('./main/table.js'))('database', this.options)
 
 module.exports = {
   version: require('./package.json').version,
   startedAt: startedAt,
-  Table: require('./main/table.js')
+  uptime: Date.now()-startedAt,
+  Table: require('./main/table.js'),
+  set: (key, value) => db.set(key, value),
+  get: (key) => db.get(key),
+  all: () => db.all(),
+  startsWith: (search) => db.startsWith(search),
+  has: (key) => db.has(key),
+  type: (key) => db.type(key),
+  add: (key, amount) => db.add(key, amount),
+  subtract: (key, amount) => db.subtract(key, amount),
+  delete: (key) => db.delete(key),
+  deleteAll: () => db.deleteTable(),
+  import: (data) => db.import(data),
+  importQuick: (data) => db.importQuick(data),
+  options: (options) => this.options = options
 }
