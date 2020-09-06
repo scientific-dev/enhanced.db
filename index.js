60ff5f97a370cea990c08bfd691ba944fd4082e0
Science Spot <69678617+Scientific-Guy@users.noreply.github.com>
2020-09-06 19:23:11 +0530

    Fixed Sqlite Errors

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
 const fs = require('fs')
 const startedAt = Date.now()
-let db = new (require('./main/table.js'))('database', this.options)
+
+let db = (ops) => new (require('./main/table.js'))('database', ops)
 
 module.exports = {
+  options: (options) => this.options = options,
   version: require('./package.json').version,
   startedAt: startedAt,
   uptime: Date.now()-startedAt,
   Table: require('./main/table.js'),
-  set: (key, value) => db.set(key, value),
-  get: (key) => db.get(key),
-  all: () => db.all(),
-  startsWith: (search) => db.startsWith(search),
-  has: (key) => db.has(key),
-  type: (key) => db.type(key),
-  add: (key, amount) => db.add(key, amount),
-  subtract: (key, amount) => db.subtract(key, amount),
-  delete: (key) => db.delete(key),
-  deleteAll: () => db.deleteTable(),
-  latency: () => db.latency(),
-  import: (data) => db.import(data),
-  importQuick: (data) => db.importQuick(data),
-  options: (options) => db = new (require('./main/table.js'))('database', options)
+  set: (key, value) => db(this.options).set(key, value),
+  get: (key) => db(this.options).get(key),
+  all: () => db(this.options).all(),
+  startsWith: (search) => db(this.options).startsWith(search),
+  has: (key) => db(this.options).has(key),
+  type: (key) => db(this.options).type(key),
+  add: (key, amount) => db(this.options).add(key, amount),
+  subtract: (key, amount) => db(this.options).subtract(key, amount),
+  push: (key, value) => db(this.options).push(key, value),
+  includes: (key, value) => db(this.options).includes(key, value),
+  delete: (key) => db(this.options).delete(key),
+  deleteAll: () => db(this.options).deleteTable(),
+  latency: () => db(this.options).latency(),
+  import: (data) => db(this.options).import(data),
+  importQuick: (data) => db(this.options).importQuick(data)
 }
