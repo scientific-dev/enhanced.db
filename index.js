508e5db5b8c0455bbb80247bcda790be0f052520
Science Spot <69678617+Scientific-Guy@users.noreply.github.com>
2020-09-19 13:31:12 +0530

    v0.3.4

--- index.js
+++ index.js
@@ -6,28 +6,28 @@
 const fs = require('fs')
 const startedAt = Date.now()
 
-let db = (ops) => new (require('./main/table.js'))('database', ops)
+this.db = new (require('./main/table.js'))('database')
 
 module.exports = {
-  options: (options) => this.options = options,
+  options: (options) => this.db = new (require('./main/table.js'))('database', options),
   version: require('./package.json').version,
   startedAt: startedAt,
   Table: require('./main/table.js'),
   Read: require('./main/read.js'),
-  set: (key, value) => db(this.options).set(key, value),
-  get: (key) => db(this.options).get(key),
-  fetch: (key) => db(this.options).fetch(key),
-  all: () => db(this.options).all(),
-  startsWith: (search) => db(this.options).startsWith(search),
-  has: (key) => db(this.options).has(key),
-  type: (key) => db(this.options).type(key),
-  add: (key, amount) => db(this.options).add(key, amount),
-  subtract: (key, amount) => db(this.options).subtract(key, amount),
-  push: (key, value) => db(this.options).push(key, value),
-  includes: (key, value) => db(this.options).includes(key, value),
-  is: (key, value) => db(this.options).is(key, value),
-  delete: (key) => db(this.options).delete(key),
-  deleteAll: () => db(this.options).deleteTable(),
-  import: (data) => db(this.options).import(data),
-  importQuick: (data) => db(this.options).importQuick(data)
+  set: (key, value) => this.db.set(key, value),
+  get: (key) => this.db.get(key),
+  fetch: (key) => this.db.fetch(key),
+  all: () => this.db.all(),
+  startsWith: (search) => this.db.startsWith(search),
+  has: (key) => this.db.has(key),
+  type: (key) => this.db.type(key),
+  add: (key, amount) => this.db.add(key, amount),
+  subtract: (key, amount) => this.db.subtract(key, amount),
+  push: (key, value) => this.db.push(key, value),
+  includes: (key, value) => this.db.includes(key, value),
+  is: (key, value) => this.db.is(key, value),
+  delete: (key) => this.db.delete(key),
+  deleteAll: () => this.db.deleteTable(),
+  import: (data) => this.db.import(data),
+  importQuick: (data) => this.db.importQuick(data)
 }
