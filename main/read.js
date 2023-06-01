b5b3de520b4e1a747f8d56726ead901ac6eb56af
ESLint <lint-action@samuelmeuli.com>
2023-06-01 22:44:35 +0000

    使用 ESLint 改進代碼質量

--- main/read.js
+++ main/read.js
@@ -1,4 +1,4 @@
-const Base = require("./base.js");
+const Base = require('./base.js')
 
 /**
  * Read EDB Sqlite files and will return data!
@@ -13,13 +13,13 @@ class Read {
    * const data = new Read('myOldFile.sqlite', { table: 'myCustomTable' })
    */
 
-  constructor(filename, options = {}) {
-    if (!filename) throw new Error("Missing filepath or filename!");
-    if (!options.table) options.table = "database";
+  constructor (filename, options = {}) {
+    if (!filename) throw new Error('Missing filepath or filename!')
+    if (!options.table) options.table = 'database'
 
-    this.filename = filename;
-    this.options = options;
-    this.base = new Base(this.options.table, this.filename);
+    this.filename = filename
+    this.options = options
+    this.base = new Base(this.options.table, this.filename)
   }
 
   /**
@@ -27,8 +27,8 @@ class Read {
    * @example const renderedData = data.get() // Now you have got all the data in object[] form
    */
 
-  get() {
-    return this.base.all();
+  get () {
+    return this.base.all()
   }
 }
 
@@ -36,4 +36,4 @@ class Read {
  * Export the table
  */
 
-module.exports = Read;
+module.exports = Read
